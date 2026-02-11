edeec8d484d8b9910225d6bf6faceecde2891caa
AvazxonAD <asqarovavazxon77@gmail.com>
2026-02-11 20:13:14 +0500

    teacher

--- src/server.js
+++ src/server.js
@@ -16,7 +16,6 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true }));
-
 app.use(responseHandler);
 
 app.use(i18next, (req, res, next) => {
